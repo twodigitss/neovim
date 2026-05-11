734f685e5c2359e5a7048d92f32b612022c8b9b5
twodigitss <torrescervantesle@gmail.com>
2026-05-10 23:13:42 -0600

    added todo comments

--- lua/plugins/all.lua
+++ lua/plugins/all.lua
@@ -16,5 +16,21 @@ return {
 
   { 'nvim-mini/mini.pairs', opts={} }, --autoclose ([{}])
   { 'windwp/nvim-ts-autotag', opts={} }, -- autoclose but for html
+  { "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      signs = true,
+      sign_priority = 8,
+      keywords = {
+        FIX =  { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+      },
+    }
+  }
 
 }
